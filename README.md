c5eda6b0f348bc8003cc22ec84bf8347c7ffc836
cpatrickalves <cpatrickalves@gmail.com>
2019-12-26 14:14:00 -0300

    Upload the project

--- README.md
+++ README.md
@@ -1,2 +1,23 @@
-# simple-flask-api
-A simple REST API with Python and Flask to retrieve book data from a Sqlite3 database.
+# Simple Flask API
+
+This project presents a simple REST API with Python and Flask to retrieve book data from a Sqlite3 database.
+
+### How to use
+
+1. Clone this project
+2. run pip install pipenv
+3. pipenv install
+4. run *python api.py*
+
+# API request examples:
+To access the data, just open the browser and access the API as the examples below.
+
+Get all books:
+``` http://127.0.0.1:5000/api/v2/resources/books/all```
+
+Get books were the author is *Connie Willis*
+```http://127.0.0.1:5000/api/v2/resources/books?author=Connie+Willis```
+
+Get books published in 2010
+``` http://127.0.0.1:5000/api/v2/resources/books?published=2010```
+
---
+++ api.py
@@ -0,0 +1,76 @@
+from flask import Flask, request, jsonify
+import sqlite3
+import os
+
+
+# Init app
+app = Flask(__name__)
+
+def dict_factory(cursor, row):
+    d = {}
+    for idx, col in enumerate(cursor.description):
+        d[col[0]] = row[idx]
+    return d
+
+# Flask maps HTTP requests to Python functions.
+# The process of mapping URLs to functions is called routing.
+@app.route('/', methods=['GET'])
+def home():
+    return "<h1>Distant Reading Archive</h1><p>This is a prototype API</p>"
+
+# A route to return all of available entries i our catalog.
+@app.route('/api/v2/resources/books/all', methods=['GET'])
+def api_all():
+    db_path = os.path.join('db', 'books.db')    
+    conn = sqlite3.connect(db_path)
+    # returns items from the database as dictionaries rather than lists
+    conn.row_factory = dict_factory
+    cur = conn.cursor()
+    all_books = cur.execute('SELECT * FROM books;').fetchall()
+
+    return jsonify(all_books)
+
+@app.errorhandler(404)
+def page_not_found(e):
+    return "<h1>404</h1><p>The resource could not be found</p>", 404
+
+@app.route('/api/v2/resources/books', methods=['GET'])
+def api_filter():
+    query_parameters = request.args
+
+    id = query_parameters.get('id')
+    published = query_parameters.get('published')
+    author = query_parameters.get('author')
+
+    query = 'SELECT * FROM books WHERE'
+    to_filter = []
+
+    if id:
+        query += ' id=? AND'
+        to_filter.append(id)
+    
+    if published:
+        query += ' published=? AND'
+        to_filter.append(published)
+
+    if author:
+        query += ' author=? AND'
+        to_filter.append(author)
+
+    if not(id or published or author):
+        return page_not_found(404)
+
+    query = query[:-4] + ';'
+
+    db_path = os.path.join('db', 'books.db')    
+    conn = sqlite3.connect(db_path)
+    conn.row_factory = dict_factory
+    cur = conn.cursor()
+
+    results = cur.execute(query, to_filter).fetchall()
+
+    return jsonify(results)
+
+# A method that runs the application server.
+if __name__ == "__main__":
+    app.run(debug=True)
